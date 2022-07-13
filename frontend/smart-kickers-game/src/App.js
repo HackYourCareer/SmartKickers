a5479b41ed40fb3c0c4de146f08c5609788ba12d
marekkawalski <marek.kawalski@gmail.com>
2022-07-13 17:23:17 +0200

    Add a very simple frontend which displays game result (not fully working)

---
+++ frontend/smart-kickers-game/src/App.js
@@ -0,0 +1,42 @@
+import { useEffect } from 'react'
+import './App.css'
+
+import * as WebSocket from "websocket"
+
+function App() {
+
+  var blueScore = 0
+  var whiteScore = 0
+
+  useEffect(() => {
+    const socket = new WebSocket.w3cwebsocket('ws://localhost:3000');
+
+    socket.onopen = function () {
+      console.log("connected");
+      socket.onmessage = (msg) => {
+        msg = JSON.parse(msg.toString())
+        if (msg.type === "blueGoal") {
+          blueScore++
+        }
+        if (msg.type === "whiteGoal") {
+          whiteScore++
+        }
+     
+        console.log(msg);
+      };
+    };
+  });
+
+  return (
+    <>
+      <h1>Smart Kickers</h1>
+      <div className="game-result-container">
+        Blue: {blueScore}
+        <br></br>
+        White: {whiteScore}
+      </div>
+    </>
+  )
+}
+
+export default App
--- main.go
+++ main.go
@@ -20,12 +20,14 @@ var goalsOne = 0
 var goalsTwo = 0
 
 func echo(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	c, err := upgrader.Upgrade(w, r, nil)
 
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
+
 	defer c.Close()
 
 	for {
@@ -50,8 +52,8 @@ func echo(w http.ResponseWriter, r *http.Request) {
 			if strings.Contains(string(message), "2") {
 				goalsTwo++
 			}
-
 		}
+
 	}
 }
 
