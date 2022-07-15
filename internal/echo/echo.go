ca1d8475b0a4d07389bde102287a1bddb35df80a
Filip Gołyszny <filip.golyszny@sap.com>
2022-07-15 16:04:13 +0200

    echo function refactor

--- internal/echo/echo.go
+++ internal/echo/echo.go
@@ -10,57 +10,78 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{}
-
 var goalsWhite, goalsBlue int
 
-func Echo(w http.ResponseWriter, r *http.Request) {
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+//	Create a initial response and send back game id json
+func handleInitial(mt int, c *websocket.Conn, dm messages.DispatcherMsg) {
+	//	package id in json
+	message := messages.DispatcherResMsg{
+		GameId: dm.TableId,
+	}
+
+	msg, err := json.Marshal(message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//	Send json back through connected websocket
+	c.WriteMessage(mt, msg)
+}
+
+//	Add point if message had a goal field, and print out score
+func handleGoal(team int) {
+
+	if team == 1 {
+		goalsWhite++
+	}
+	if team == 2 {
+		goalsBlue++
+	}
+
+	log.Println("Team 1 score: " + strconv.Itoa(goalsWhite) + " Team 2 score: " + strconv.Itoa(goalsBlue))
+}
+
+//	Upgrade the http to websocket connection and check for errors, return the upgraded connection
+func connect(w http.ResponseWriter, r *http.Request) *websocket.Conn {
+	var upgrader websocket.Upgrader
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true } //	Allow all connections by default
 	c, err := upgrader.Upgrade(w, r, nil)
 
 	if err != nil {
-		log.Print("upgrade:", err)
-		return
+		log.Fatalln("upgrade:", err)
 	}
 
-	defer c.Close()
+	return c
+}
+
+func Echo(w http.ResponseWriter, r *http.Request) {
+	c := connect(w, r)
+
+	defer c.Close() //	Close connection when infinite loop below exits
 
 	for {
-		///test
+		//	Receive message from connection, check for errors
 		mt, message, err := c.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
-			break
+			return
 		}
 
-		var dispatcherMsg messages.DispatcherReqMsg
-		er := json.Unmarshal([]byte(message), &dispatcherMsg)
-
-		if er != nil {
+		//	Resolve given json into provided struct
+		var dm messages.DispatcherMsg
+		err = json.Unmarshal([]byte(message), &dm)
+		if err != nil {
 			log.Println(err)
 		}
 
-		if dispatcherMsg.MsgType == "INITIAL" {
-			message := messages.DispatcherResMsg{
-				GameId: dispatcherMsg.TableId,
-			}
-			msg, err := json.Marshal(message)
-
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			c.WriteMessage(mt, msg)
+		//	Establish a connection if initial message is received
+		if dm.MsgType == "INITIAL" {
+			handleInitial(mt, c, dm)
 		}
 
-		if dispatcherMsg.Goal == 1 {
-			goalsWhite++
+		if dm.Goal != 0 {
+			handleGoal(dm.Goal)
 		}
-
-		if dispatcherMsg.Goal == 2 {
-			goalsBlue++
-		}
-
-		log.Println("Team 1 score: " + strconv.Itoa(goalsWhite) + " Team 2 score: " + strconv.Itoa(goalsBlue))
 	}
+
 }
--- pkg/messages/dispatcher.go
+++ pkg/messages/dispatcher.go
@@ -1,15 +1,16 @@
 package messages
 
-type DispatcherReqMsg struct {
-	MsgType string `json:"type"`
-	Origin string `json:"origin"`
-	TableId int `json:"id"`
-	X float64 `json:"x"`
-	Y float64`json:"y"`
-	Goal int `json:"goal"`
+type DispatcherMsg struct {
+	MsgType   string  `json:"type"`
+	Origin    string  `json:"origin"`
+	TableId   int     `json:"id"`
+	X         float64 `json:"x"`
+	Y         float64 `json:"y"`
+	Timestamp string  `json:"timestamp"`
+	Goal      int     `json:"goal"`
 }
 
 type DispatcherResMsg struct {
-	GameId int `json:"start"`
+	GameId    int `json:"start"`
 	GameEnded int `json:"end"`
 }
