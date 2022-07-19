0ecabc796ae93fe770f29324260414271a601e56
Marek Michali <marek.michali@sap.com>
2022-07-19 09:04:01 +0200

    Small refactor

--- main.go
+++ main.go
@@ -9,17 +9,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var nodeAddr = flag.String("addr", "0.0.0.0:3000", "nodeAddr")
+var nodeAddr = flag.String("addr1", "0.0.0.0:3000", "nodeAddr")
 var reactAddr = flag.String("addr2", "0.0.0.0:3006", "reactAddr")
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", echo.Echo)
-	r.HandleFunc("/shot", echo.ReceiveShotMsg)
-	go http.ListenAndServe(*nodeAddr, r)
+	r1 := mux.NewRouter()
+	r1.HandleFunc("/", echo.Echo)
+	r1.HandleFunc("/shot", echo.ReceiveShotMsg)
+	go http.ListenAndServe(*nodeAddr, r1)
 
 	r2 := mux.NewRouter()
 	r2.HandleFunc("/csc", echo.ClientServerConn)
