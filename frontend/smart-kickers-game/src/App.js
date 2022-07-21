c67bc6c247adaa1b2e4c0306966e47885d12ac2c
mwisnia97 <mwisnia97@gmail.com>
2022-07-21 10:09:46 +0200

    test

--- frontend/smart-kickers-game/src/App.js
+++ frontend/smart-kickers-game/src/App.js
@@ -1,26 +1,25 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import {Button} from "./components/button"
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Button } from "./components/button";
 
 function App() {
-
   let [blueScore, setBlueScore] = useState(0);
   let [whiteScore, setWhiteScore] = useState(0);
 
   useEffect(() => {
-    const socket = new WebSocket('ws://localhost:3006/csc');
+    const socket = new WebSocket("ws://localhost:3006/csc");
     //const socket = new WebSocket("ws://localhost:3000")
     socket.onopen = function () {
       console.log("connected");
       //send to server
-      socket.send("Hello from client")
+      socket.send("Hello from client");
       socket.onmessage = (msg) => {
         console.log(msg);
-        msg = JSON.parse(msg.data)
-        console.log(msg)
-        setBlueScore(msg.blueScore)
-        setWhiteScore(msg.whiteScore)
-     
+        msg = JSON.parse(msg.data);
+        console.log(msg);
+        setBlueScore(msg.blueScore);
+        setWhiteScore(msg.whiteScore);
+
         console.log(msg);
       };
     };
@@ -31,12 +30,14 @@ function App() {
       <h1>Smart Kickers</h1>
       <div className="game-result-container" data-testid="blue-team-score">
         Blue: {blueScore}
-          {"   "}
+        {"   "}
         White: {whiteScore}
       </div>
-      <center><Button >Reset game</Button></center>
+      <center>
+        <Button>Reset game</Button>
+      </center>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
