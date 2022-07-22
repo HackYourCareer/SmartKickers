e7838abcf3e8b9def31d56d57725cb198573e440
Kacper Malachowski <kacper.malachowski@sap.com>
2022-07-22 11:25:16 +0200

    Refactor button.js, set default styles to primary medium button

--- frontend/smart-kickers-game/src/components/button.js
+++ frontend/smart-kickers-game/src/components/button.js
@@ -1,25 +1,16 @@
 import React from "react";
 import "./button.css";
 
-const STYLES = ["btn--primary", "btn--outline"];
-const SIZES = ["btn--medium", "btn--large"];
-
 export const Button = ({
   children,
   type,
   onClick,
-  buttonStyle,
-  buttonSize,
+  className = "btn--primary btn--medium",
                        }) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
-
-  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
     <button
-      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      className={`btn ${className}`}
       onClick={onClick}
       type={type}
     >
