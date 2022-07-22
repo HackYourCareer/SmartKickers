86dda8ee5fe5059b86fefb946d4626b3488c862f
MichalKalke <michal.kalke@sap.com>
2022-07-22 15:33:13 +0200

    Game model with methods

--- internal/model/game.go
+++ internal/model/game.go
@@ -1,1 +1,20 @@
 package model
+
+type GameScore struct {
+	BlueScore  int `json:"blueScore"`
+	WhiteScore int `json:"whiteScore"`
+}
+
+func (gameScore *GameScore) ResetScore() {
+	gameScore.BlueScore = 0
+	gameScore.WhiteScore = 0
+}
+
+func (gameScore *GameScore) AddGoal(teamID int) {
+	switch teamID {
+	case 1:
+		gameScore.WhiteScore++
+	case 2:
+		gameScore.BlueScore++
+	}
+}
