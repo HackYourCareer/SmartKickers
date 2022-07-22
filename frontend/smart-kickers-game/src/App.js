16042933baf70a56247cd6abf2fdc4bd565e97d2
Kacper Malachowski <kacper.malachowski@sap.com>
2022-07-22 11:11:49 +0200

    Move team scores to separate elements
    Summary of changes
      1. Move Blue and White teams scores to separete html element
      2. Adjust styles for this changes by making container flex and adding padding to items

--- frontend/smart-kickers-game/src/App.js
+++ frontend/smart-kickers-game/src/App.js
@@ -4,24 +4,19 @@ import {Button} from "./components/button"
 
 function App() {
 
-  let [blueScore, setBlueScore] = useState(0);
-  let [whiteScore, setWhiteScore] = useState(0);
+  const [blueScore, setBlueScore] = useState(0);
+  const [whiteScore, setWhiteScore] = useState(0);
 
   useEffect(() => {
     const socket = new WebSocket('ws://localhost:3006/csc');
     //const socket = new WebSocket("ws://localhost:3000")
     socket.onopen = function () {
-      console.log("connected");
       //send to server
       socket.send("Hello from client")
       socket.onmessage = (msg) => {
-        console.log(msg);
         msg = JSON.parse(msg.data)
-        console.log(msg)
         setBlueScore(msg.blueScore)
         setWhiteScore(msg.whiteScore)
-     
-        console.log(msg);
       };
     };
   });
@@ -29,14 +24,15 @@ function App() {
   return (
     <>
       <h1>Smart Kickers</h1>
-      <div className="game-result-container" data-testid="blue-team-score">
-        Blue: {blueScore}
-          {"   "}
-        White: {whiteScore}
+      <div className="game-result-container">
+        <p className="game-result-item">Blue: {blueScore}</p>
+        <p className="game-result-item">White: {whiteScore}</p>
       </div>
-      <center><Button >Reset game</Button></center>
+      <center>
+        <Button>Reset game</Button>
+      </center>
     </>
-  )
+  );
 }
 
 export default App
