1693712b3d198ba8be8abf1d4aeadb56dc4470dd
Kacper Malachowski <kacper.malachowski@sap.com>
2022-07-22 11:09:38 +0200

    Move App.spec.js udner tests directory and add helper function
    Summary of changes:
     1. Move tests files under tests directory in src
     2. Create helper function getElementWHichContain in helpers.js file for better readibility

---
+++ frontend/smart-kickers-game/src/tests/App.spec.js
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import WS from "jest-websocket-mock";
+import { getElementWhichContain } from './helpers';
+import App from '../App'
+
+let ws
+describe('<App />', () => { 
+
+  beforeEach(() => {
+    ws = new WS("ws://localhost:3006/csc")
+  })
+
+  afterEach(() => {
+    WS.clean()
+  })
+
+  it('should render correctly',  async () => {
+    render(<App />);
+
+    expect(getElementWhichContain("Blue:")).toBeDefined()
+    expect(getElementWhichContain("White:")).toBeDefined();
+  })
+
+  it('should update score on score message', async () => {
+    render(<App />)
+    await ws.connected
+
+    ws.send(JSON.stringify({blueScore: 1, whiteScore: 1}));
+
+    expect(getElementWhichContain("Blue:")).toHaveTextContent("1");
+    expect(getElementWhichContain("White:")).toHaveTextContent("1");
+  })
+
+  it('should update score on score message', async () => {
+    render(<App />)
+    await ws.connected
+
+    ws.send(JSON.stringify({ blueScore: 10, whiteScore: 14 }));
+
+     expect(getElementWhichContain("Blue:")).toHaveTextContent("10");
+     expect(getElementWhichContain("White:")).toHaveTextContent("14");
+  })
+})
