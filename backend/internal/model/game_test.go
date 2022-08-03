17463e0f7b902c4933aa32841a22d66dec3b6dcb
MichalKalke <michal.kalke@sap.com>
2022-08-03 11:24:41 +0200

    Add function in game.go to substract the goal of the team

--- backend/internal/model/game_test.go
+++ backend/internal/model/game_test.go
@@ -59,3 +59,39 @@ func TestAddGoal(t *testing.T) {
 		})
 	}
 }
+
+func Test_game_SubGoal(t *testing.T) {
+	game := &game{gameScore{0, 2}}
+
+	type args struct {
+		name               string
+		teamID             int
+		expectedBlueScore  int
+		expectedWhiteScore int
+		expectedError      string
+	}
+	tests := []args{
+		{name: "should decrement team white score by one", teamID: teamWhite, expectedBlueScore: 2, expectedWhiteScore: 0, expectedError: ""},
+		{name: "should decrement team blue score by one", teamID: teamBlue, expectedBlueScore: 1, expectedWhiteScore: 1, expectedError: ""},
+		{name: "should not decrement team blue score by one", teamID: teamWhite, expectedBlueScore: 2, expectedWhiteScore: 0, expectedError: ""},
+		{name: "should cause an error when invalid team ID", teamID: -1, expectedBlueScore: 2, expectedWhiteScore: 1, expectedError: "bad team ID"},
+	}
+
+	for id, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			game.score.WhiteScore = 1
+			game.score.BlueScore = 2
+			if id == 2 {
+				game.score.WhiteScore = 0
+			}
+			err := game.SubGoal(tt.teamID)
+			if tt.expectedError == "" {
+				assert.Nil(t, err)
+			} else {
+				assert.EqualError(t, err, tt.expectedError)
+			}
+			assert.Equal(t, game.score.BlueScore, tt.expectedBlueScore, "blue team score changes incorrectly")
+			assert.Equal(t, game.score.WhiteScore, tt.expectedWhiteScore, "white team score changes incorrectly")
+		})
+	}
+}
