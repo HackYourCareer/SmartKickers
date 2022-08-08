1633d5ffd4cbeb8de51ef284ec905148abf70e91
Kacper Malachowski <kacper.malachowski@sap.com>
2022-08-08 15:52:07 +0200

    Rename post to put and fix error handling

--- frontend/smart-kickers-game/src/apis/goalManipulation.js
+++ frontend/smart-kickers-game/src/apis/goalManipulation.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import config from '../config';
 
-export async function pointsManipulation(teamID, action) {
-  try {
-    await axios.post(`${config.apiBaseUrl}/goal?action=${action}&team=${teamID}`);
-  } catch (error) {
-    alert(error);
-  }
+export function pointsManipulation(teamID, action) {
+  axios.put(`${config.apiBaseUrl}/goal?action=${action}&team=${teamID}`).catch((err) => {
+    alert(err);
+  });
 }
