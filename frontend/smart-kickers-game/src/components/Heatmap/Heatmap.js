47552188dcc1f3f55daea111cd00dfdfd9bd5dbe
Marek Kawalski <56251920+marekkawalski@users.noreply.github.com>
2022-08-19 14:19:09 +0200

    Refactor heatmap component

--- frontend/smart-kickers-game/src/components/Heatmap/Heatmap.js
+++ frontend/smart-kickers-game/src/components/Heatmap/Heatmap.js
@@ -3,34 +3,24 @@ import HeatMap from 'react-heatmap-grid';
 import './Heatmap.css';
 
 function Heatmap({ heatMapTable }) {
-  const xLabels = new Array(Object.keys(heatMapTable.data).length).fill(0).map((_, i) => ``);
-
-  // Display only even labels
-  const xLabelsVisibility = new Array(24).fill(0).map((_, i) => (i % 2 === 0 ? true : false));
-
-  let i = 0;
-  const yLabels = new Array(Object.keys(heatMapTable.data).length).fill(0).map((_, i) => ``);
-  const data = new Array(yLabels.length).fill(0).map(() => new Array(xLabels.length).fill(0).map(() => Math.floor(i++)));
-  console.log(heatMapTable);
-
+  const heatmapDim = Object.keys(heatMapTable.data).length;
+  const array = new Array(heatmapDim).fill(0).map(() => '');
   let numbersCopy = JSON.parse(JSON.stringify(heatMapTable.data));
 
-  let n = 100;
-  for (let i = 0; i < n; i++)
-    for (let j = 0; j <= i; j++) numbersCopy[i][j] = numbersCopy[i][j] + numbersCopy[j][i] - (numbersCopy[j][i] = numbersCopy[i][j]);
+  for (let i = 0; i < heatmapDim; i++)
+    for (let j = 0; j <= i; j++) {
+      numbersCopy[i][j] = numbersCopy[i][j] + numbersCopy[j][i] - numbersCopy[j][i];
+      numbersCopy[j][i] = numbersCopy[i][j];
+    }
 
   return (
     <div className="heatmap-parent">
       <div className="heatmap-container">
         <HeatMap
-          xLabels={xLabels}
-          yLabels={yLabels}
-          xLabelsLocation={'bottom'}
-          xLabelsVisibility={xLabelsVisibility}
-          xLabelWidth={60}
+          xLabels={array}
+          yLabels={array}
           data={numbersCopy}
-          height={100}
-          cellStyle={(background, value, min, max, data, x, y) => ({
+          cellStyle={(background, value, min, max) => ({
             background: `rgb(0, 151, 230, ${1 - (max - value) / (max - min)})`,
             fontSize: '0px',
             color: '#444',
