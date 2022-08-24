b96bd98216e6e8fc3503bd469102885bc9f2b86b
Marek Kawalski <56251920+marekkawalski@users.noreply.github.com>
2022-08-24 10:46:03 +0200

    Update backend/internal/controller/adapter/shot_message_test.go
    Co-authored-by: Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>

--- backend/internal/controller/adapter/shot_message_test.go
+++ backend/internal/controller/adapter/shot_message_test.go
@@ -58,6 +58,7 @@ func TestUnpackShotMsg(t *testing.T) {
 			expectedError:  "missing shot parameters",
 		},
 	}
+	
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var rawSlice []json.RawMessage
