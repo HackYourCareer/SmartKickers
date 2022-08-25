50189c76691b902056f9f1873816030fde9aa455
Marek Kawalski <56251920+marekkawalski@users.noreply.github.com>
2022-08-25 13:40:32 +0200

    Change result type from let to const

--- frontend/smart-kickers-game/src/apis/goalManipulation.js
+++ frontend/smart-kickers-game/src/apis/goalManipulation.js
@@ -3,7 +3,7 @@ import config from '../config';
 
 export async function pointsManipulation(teamID, action) {
   try {
-    let result = await axios.post(`${config.apiBaseUrl}/goal?action=${action}&team=${teamID}`);
+    const result = await axios.post(`${config.apiBaseUrl}/goal?action=${action}&team=${teamID}`);
 
     return {
       status: result.status,
