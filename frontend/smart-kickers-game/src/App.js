fdafe57cf8588c2b091556cd258614e24465f643
MichalKalke <michal.kalke@sap.com>
2022-08-26 15:55:32 +0200

    Create array with timestamps and ID's of teams which scored goals

--- frontend/smart-kickers-game/src/App.js
+++ frontend/smart-kickers-game/src/App.js
@@ -4,12 +4,14 @@ import { resetGame } from './apis/resetGame';
 import GameStatistics from './components/Game/GameStatistics/GameStatistics.js';
 import config from './config';
 import CurrentGameplay from './components/Game/CurrentGameplay/CurrentGameplay';
+import { Goal, TeamID } from './constants/score.js';
 
 function App() {
   const [blueScore, setBlueScore] = useState(0);
   const [whiteScore, setWhiteScore] = useState(0);
   const [isStatisticsDisplayed, setIsStatisticsDisplayed] = useState(false);
   const [finalScores, setFinalScores] = useState({ blue: 0, white: 0 });
+  const goalsArray = new Array();
   useEffect(() => {
     const socket = new WebSocket(`${config.wsBaseUrl}/score`);
 
@@ -23,6 +25,16 @@ function App() {
       };
     };
   }, []);
+  useEffect(() => {
+    goalsArray.push(new Goal(TeamID.Team_blue, '10:30'));
+  }, [blueScore]);
+  useEffect(() => {
+    goalsArray.push(new Goal(TeamID.Team_white, '12:30'));
+  }, [whiteScore]);
+
+  const handleStartGame = () => {
+    handleResetGame();
+  };
 
   const handleResetGame = () => {
     resetGame().then((data) => {
@@ -30,6 +42,7 @@ function App() {
     });
   };
   const handleEndGame = () => {
+    console.log(goalsArray);
     setFinalScores({ blue: blueScore, white: whiteScore });
     setIsStatisticsDisplayed(!isStatisticsDisplayed);
   };
@@ -39,7 +52,13 @@ function App() {
       {isStatisticsDisplayed ? (
         <GameStatistics finalScores={finalScores} setIsStatisticsDisplayed={setIsStatisticsDisplayed} handleResetGame={handleResetGame} />
       ) : (
-        <CurrentGameplay blueScore={blueScore} whiteScore={whiteScore} handleResetGame={handleResetGame} handleEndGame={handleEndGame} />
+        <CurrentGameplay
+          blueScore={blueScore}
+          whiteScore={whiteScore}
+          handleStartGame={handleStartGame}
+          handleResetGame={handleResetGame}
+          handleEndGame={handleEndGame}
+        />
       )}
     </>
   );
