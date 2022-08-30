015bf2baff7ad9a7b5a5b38cdbf5b1814f4c27a1
Marek Michali <marek.michali@sap.com>
2022-08-30 11:15:34 +0200

    Add heatmap sending to frontend

--- backend/internal/controller/server/msg_handler.go
+++ backend/internal/controller/server/msg_handler.go
@@ -69,6 +69,7 @@ func (s server) createResponse(reader io.Reader) ([]byte, error) {
 		return nil, err
 	case adapter.MsgPosition:
 		log.Trace("X coord of the ball: ", message.X, " Y coord of the ball: ", message.Y)
+		s.game.IncrementHeatmap(message.X, message.Y)
 		return nil, nil
 	default:
 		return nil, fmt.Errorf("unrecognized message type %d", message.Category)
@@ -207,7 +208,7 @@ func (s server) ShotParametersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s server) ShowStatsHandler(w http.ResponseWriter, r *http.Request) {
-	response, err := json.Marshal(s.game.GetShotsData().Fastest)
+	response, err := json.Marshal(s.game.GetGameStats())
 	if err != nil {
 		log.Error(err)
 		err = writeHTTPError(w, http.StatusInternalServerError, "Couldn't get fastest shot")
