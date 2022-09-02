35d97b24274b3f8cf8cfda6f03be1e74f6449288
Marek Kawalski <56251920+marekkawalski@users.noreply.github.com>
2022-09-02 14:57:25 +0200

    Move mirror heatmap outside useHeatmap

--- frontend/smart-kickers-game/src/hooks/useHeatmap.js
+++ frontend/smart-kickers-game/src/hooks/useHeatmap.js
@@ -2,6 +2,27 @@ import useAxios from 'axios-hooks';
 import config from '../config';
 import { useMemo } from 'react';
 
+function mirrorHeatmap(data) {
+  const heatmapDim = data.Heatmap.length;
+  const array = new Array(heatmapDim).fill('');
+  const numbersCopy = JSON.parse(JSON.stringify(data.Heatmap));
+
+  const transpose = (matrix) => {
+    for (let row = 0; row < matrix.length; row++) {
+      for (let column = 0; column < row; column++) {
+        let temp = matrix[row][column];
+        matrix[row][column] = matrix[column][row];
+        matrix[column][row] = temp;
+      }
+    }
+    return matrix;
+  };
+
+  const transposedMatrix = transpose(numbersCopy);
+
+  return { array, transposedMatrix };
+}
+
 const useHeatmap = () => {
   const [{ data, loading, error }] = useAxios({
     method: 'get',
@@ -9,27 +30,6 @@ const useHeatmap = () => {
   });
 
   const heatmap = useMemo(() => (data?.Heatmap ? mirrorHeatmap() : []), [data]);
-
-  function mirrorHeatmap() {
-    const heatmapDim = data.Heatmap.length;
-    const array = new Array(heatmapDim).fill('');
-    const numbersCopy = JSON.parse(JSON.stringify(data.Heatmap));
-
-    const transpose = (matrix) => {
-      for (let row = 0; row < matrix.length; row++) {
-        for (let column = 0; column < row; column++) {
-          let temp = matrix[row][column];
-          matrix[row][column] = matrix[column][row];
-          matrix[column][row] = temp;
-        }
-      }
-      return matrix;
-    };
-
-    const transposedMatrix = transpose(numbersCopy);
-
-    return { array, transposedMatrix };
-  }
 
   return [{ loading, error, heatmap }];
 };
