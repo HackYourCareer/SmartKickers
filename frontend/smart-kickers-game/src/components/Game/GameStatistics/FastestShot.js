b2f103181cff5a9058124f9782df394e9482b316
MichalKalke <michal.kalke@sap.com>
2022-09-08 12:04:08 +0200

    Move statistics to seperate files

---
+++ frontend/smart-kickers-game/src/components/Game/GameStatistics/FastestShot.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './GameStatistics.css';
+
+function FastestShot({ blue, white, statistics }) {
+  function returnFastestShot(teamID) {
+    if (!statistics?.fastestShot) return '😞';
+    const { speed, team } = statistics.fastestShot;
+    return team === teamID ? speed.toFixed(2) + ' km/h' : '😵';
+  }
+  return (
+    <>
+      <div className="table-item">{returnFastestShot(blue)}</div>
+      <div className="table-item">fastest shot of the game</div>
+      <div className="table-item">{returnFastestShot(white)}</div>
+    </>
+  );
+}
+
+export default FastestShot;
--- frontend/smart-kickers-game/src/components/Game/GameStatistics/GameStatistics.js
+++ frontend/smart-kickers-game/src/components/Game/GameStatistics/GameStatistics.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './GameStatistics.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button } from '../../Button/Button.js';
 import { getStatistics } from '../../../apis/getStatistics.js';
 import { useEffect, useState } from 'react';
 import { TeamID } from '../../../constants/score.js';
-import StatisticsItem from './StatisticsItems/StatisticItem';
+//import StatisticsItem from './StatisticsItems/StatisticItem';
+import FinalScores from './FinalScores.js';
+import FastestShot from './FastestShot.js';
+import ManualChangedGoals from './ManualChangedGoals.js';
+import TeamIcons from './TeamIcons.js';
 
 function GameStatistics({ finalScores, onNewGameRequested }) {
   const [statistics, setStatistics] = useState(null);
@@ -15,25 +19,6 @@ function GameStatistics({ finalScores, onNewGameRequested }) {
     if (result?.error) alert(result.error);
     setStatistics(result);
   };
-
-  function returnFastestShot(teamID) {
-    if (!statistics?.fastestShot) return '😞';
-    const { speed, team } = statistics.fastestShot;
-    return team === teamID ? speed.toFixed(2) + ' km/h' : '😵';
-  }
-
-  function getManualChangedGoals(teamID) {
-    if (!statistics?.manualGoals) return;
-    return statistics.manualGoals[teamID];
-  }
-
-  function getManualSubstractedGoals(teamID) {
-    return getManualChangedGoals(teamID)?.sub || 0;
-  }
-
-  function getManualAddedGoals(teamID) {
-    return getManualChangedGoals(teamID)?.add || 0;
-  }
 
   useEffect(() => {
     handleGetStatistics();
@@ -45,29 +30,10 @@ function GameStatistics({ finalScores, onNewGameRequested }) {
         <em>Statistics</em>
       </h2>
       <div className="table-with-stats">
-        <StatisticsItem>
-          {' '}
-          <FontAwesomeIcon className="blue-team-icon" icon="fa-person" />
-          Blue
-        </StatisticsItem>
-        <StatisticsItem />
-        <StatisticsItem>
-          {' '}
-          <FontAwesomeIcon className="white-team-icon" icon="fa-person" />
-          White
-        </StatisticsItem>
-        <StatisticsItem>{finalScores.blue}</StatisticsItem>
-        <StatisticsItem>score</StatisticsItem>
-        <StatisticsItem>{finalScores.white}</StatisticsItem>
-        <StatisticsItem>{returnFastestShot(TeamID.Team_blue)}</StatisticsItem>
-        <StatisticsItem>fastest shot of the game</StatisticsItem>
-        <StatisticsItem>{returnFastestShot(TeamID.white)}</StatisticsItem>
-        <StatisticsItem>{getManualAddedGoals(TeamID.Team_blue)}</StatisticsItem>
-        <StatisticsItem>Manually added goals</StatisticsItem>
-        <StatisticsItem>{getManualAddedGoals(TeamID.Team_white)}</StatisticsItem>
-        <StatisticsItem>{getManualSubstractedGoals(TeamID.Team_blue)}</StatisticsItem>
-        <StatisticsItem>Manually substracted goals</StatisticsItem>
-        <StatisticsItem>{getManualSubstractedGoals(TeamID.Team_white)}</StatisticsItem>
+        <TeamIcons />
+        <FinalScores blue={finalScores.blue} white={finalScores.white} />
+        <FastestShot blue={TeamID.Team_blue} white={TeamID.Team_white} statistics={statistics} />
+        <ManualChangedGoals blue={TeamID.Team_blue} white={TeamID.Team_white} statistics={statistics} />
       </div>
 
       <Button
