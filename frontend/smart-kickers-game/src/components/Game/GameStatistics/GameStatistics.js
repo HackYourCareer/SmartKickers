ab72d037bc591f923dfc4968c460f63e1003ccfb
MichalKalke <michal.kalke@sap.com>
2022-09-19 12:45:10 +0200

    Remove console log

--- frontend/smart-kickers-game/src/components/Game/GameStatistics/GameStatistics.js
+++ frontend/smart-kickers-game/src/components/Game/GameStatistics/GameStatistics.js
@@ -12,7 +12,6 @@ function GameStatistics() {
 
   const handleGetStatistics = async () => {
     const result = await getStatistics();
-    console.log(result);
     result?.error ? alert(result.error) : setStatistics(result);
   };
 
