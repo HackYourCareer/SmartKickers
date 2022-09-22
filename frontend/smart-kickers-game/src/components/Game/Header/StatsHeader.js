203eb0cc748a656d3f3de4a611e36877e8b5cc22
Marek Kawalski <56251920+marekkawalski@users.noreply.github.com>
2022-09-22 06:28:19 +0200

    Disable navigate buttons when on the site to which button redirects

--- frontend/smart-kickers-game/src/components/Game/Header/StatsHeader.js
+++ frontend/smart-kickers-game/src/components/Game/Header/StatsHeader.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { Button } from '../../Button/Button';
 import { useNavigate } from 'react-router-dom';
 import './StatsHeader.css';
+import { useLocation } from 'react-router-dom';
 
 function StatsHeader() {
   const navigate = useNavigate();
+  const location = useLocation();
   return (
     <>
       <h1>Smart Kickers</h1>
       <div className="nav-buttons">
-        <Button onClick={() => navigate('/stats')}>Statistics</Button>
-        <Button onClick={() => navigate('/stats/heatmap')}>Heatmap</Button>
-        <Button onClick={() => navigate('/stats/game-history')}>Game history</Button>
+        <Button onClick={() => navigate('/stats')} disabled={location.pathname === '/stats'}>
+          Statistics
+        </Button>
+        <Button onClick={() => navigate('/stats/heatmap')} disabled={location.pathname === '/stats/heatmap'}>
+          Heatmap
+        </Button>
+        <Button onClick={() => navigate('/stats/game-history')} disabled={location.pathname === '/stats/game-history'}>
+          Game history
+        </Button>
       </div>
     </>
   );
