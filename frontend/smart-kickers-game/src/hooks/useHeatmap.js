f591d8198d700dfc1a53b98bf88b1c1b8421bae3
Marek Michali <marek.michali@sap.com>
2022-09-29 09:49:04 +0200

    Optimize heatmap generating on frontend

--- frontend/smart-kickers-game/src/hooks/useHeatmap.js
+++ frontend/smart-kickers-game/src/hooks/useHeatmap.js
@@ -5,19 +5,8 @@ import { useMemo } from 'react';
 function mirrorHeatmap(data) {
   const heatmapDim = data.length;
   const array = new Array(heatmapDim).fill('');
-  const transpose = (matrix) => {
-    const numbersCopy = structuredClone(matrix);
-    for (let row = 0; row < numbersCopy.length; row++) {
-      for (let column = 0; column < row; column++) {
-        let temp = numbersCopy[row][column];
-        numbersCopy[row][column] = numbersCopy[column][row];
-        numbersCopy[column][row] = temp;
-      }
-    }
-    return numbersCopy;
-  };
 
-  return { array, numbersCopy: transpose(data) };
+  return { array, numbersCopy: data };
 }
 
 const useHeatmap = () => {
